0897640e7596da6f255de716aa682ef90c9a88c2
Benjamin Yarmowich <Benjamin Yarmowich>
2016-09-07 10:12:22 -0400

    Switched computers

--- Civilization.java
+++ Civilization.java
@@ -2,32 +2,23 @@ import java.util.Scanner;
 public class Civilization {
   public static Scanner scan = new Scanner(System.in);
   public static String empire = null;
-<<<<<<< HEAD
   public static String [] cities = {"","","","",""};
-=======
-  public static String [] cities = {"a"};
->>>>>>> 84177aa3da1ef3655de4b8b060048dc518580df1
   public static double gold = 20.5;
   public static double resources = 30;
   public static int happiness = 10;
   public static int troops = 0;
   public static int tech = 0;
   public static int turn = 0;
-<<<<<<< HEAD
   public static int attacks = 0;
   public static int numOfCities = 1;
-=======
->>>>>>> 84177aa3da1ef3655de4b8b060048dc518580df1
   public static boolean playing = true;
     
     public static void main(String[] args) {
- 
       setUpGame();
       while(playing) {
-        startOfTurn();
         preformAction();
-<<<<<<< HEAD
         if (attacks==10||tech==20) {endGame();}
+        endOfTurn();
         playing = false;
       }
     }
@@ -56,14 +47,13 @@ public class Civilization {
     String firstCityName = scan.nextLine();
     cities[0] = firstCityName;
   }
-  public static void startOfTurn(){
+  public static void endOfTurn(){
     turn++;
     resources++;
     if (happiness>20) resources = resources + (5*numOfCities);
     gold = gold+3;
     if (resources%2 ==0) happiness++;
     else happiness = happiness -3;
-    System.out.println(status());
   }
   public static String status(){
     String currentStatus1 = null;
@@ -82,6 +72,7 @@ public class Civilization {
     return currentStatus1+currentStatus2+currentStatus3+currentStatus4;
   }
   public static void preformAction(){
+    System.out.println(status());
     boolean correct = false;
     System.out.println("On your turn you can:\nSettle a City [1]\nDemolish a City [2]\nBuild Your Army [3]\nResearch Technology [4]\nAtttack An Enemy City [5] \nor End Your Turn [6]\n");
     while (!correct){   
@@ -148,7 +139,7 @@ public class Civilization {
     boolean cont = true;
     boolean continuing = true;
     if (numOfCities==1){System.out.println("\nYou have 1 city:");}
-    else {System.out.println("You have "+numOfCities+" cities:");}
+    else {System.out.println("You have "+numOfCities+1+" cities:");}
     for (int i =0; i<=(numOfCities-1);i++) {
       System.out.println(cities[i]+" ["+i+"]");
     }
@@ -186,10 +177,7 @@ public class Civilization {
         }
         cont=false;
       }
-=======
         playing = false;
-      }
->>>>>>> 84177aa3da1ef3655de4b8b060048dc518580df1
     }
   }
   public static void buildArmy(){
@@ -214,6 +202,7 @@ public class Civilization {
         else{
           gold = gold-5;
           resources = resources-3;
+          troops++;
           System.out.println("Congradulations, you successfully trained another unit\nYou have "+troops+" units");
         }
         continuing = false;
@@ -221,14 +210,39 @@ public class Civilization {
     }
   }
   public static void researchTech(){
-
+    int research =0;
+    boolean continuing = true;
+    System.out.println("You have "+tech+" technology past the stone age\nTo develop more technology costs 50 gold and 2 resources\nYou currently have "+gold+" gold and "+resources+" resources");
+    while (continuing ==true){
+      System.out.println("Would you like to train more units?\nYes[1]\nNo [2]");
+      build = scan.nextInt();
+      scan.nextLine();
+      if (build==2) {
+        preformAction();
+        continuing =false;
+      }
+      else if (build==1){
+        if (gold<5) {
+          System.out.println("You do not have enough gold");
+        }
+        else if (resources<3) {
+          System.out.println("You do not have enough resources");
+        }
+        else{
+          gold = gold-5;
+          resources = resources-3;
+          troops++;
+          System.out.println("Congradulations, you successfully trained another unit\nYou have "+troops+" units");
+        }
+        continuing = false;
+      }
+    }
   }
   public static void attackCity(){
 
   }
   public static void endTurn(){
 
-<<<<<<< HEAD
   }
   public static void endGame(){
     String endStatus2 = null;
@@ -244,101 +258,4 @@ public class Civilization {
     if (tech>0){endStatus4 = "and "+tech+" technologies past the stone age\n";}
     System.out.println(endStatus2+endStatus3+endStatus4);
   }
-} 
-=======
-  public static void setUpGame(){
-    boolean correct = false;
-    System.out.println("Would you like to play as America [1], The Zulu [2], England [3], or China [4]\nPlease return a value between 1 and 4");
-    while (!correct){
-      int empireNum = scan.nextInt();
-      if (empireNum == 1) {
-        empire = "American";
-        correct = true;}
-      else if (empireNum == 2){
-        empire = "Zulu";
-        correct = true;}
-      else if (empireNum == 3){
-        empire = "English";
-        correct = true;}
-      else if (empireNum == 4){
-        empire = "Chinese";
-        correct = true;}
-      else System.out.println ("Please enter a number 1 through 4");
-    }
-    scan.nextLine();
-    System.out.println("What would you like to call your first city?");
-    String firstCityName = scan.nextLine();
-    cities[0] = firstCityName;
-  }
-  public static void startOfTurn(){
-    turn++;
-    resources++;
-    if (happiness>20) resources = resources + (5*cities.length);
-    gold = gold+3;
-    if (resources%2 ==0) happiness++;
-    else happiness = happiness -3;
-    System.out.println(status());
-  }
-  public static String status(){
-   	String currentStatus1 = null;
-   	String currentStatus2 = null;
-   	String currentStatus3 = null;
-   	String currentStatus4 = null;
-    if (cities.length>=2){ currentStatus1 = "\n\nIt is turn "+turn+" and you, the "+empire+ " empire, have: \n"+cities.length+" cities\n"+gold+ " thousand gold\n"+resources+" tons of resources\n";}
-    if (cities.length==1){ currentStatus1 = "\n\nIt is turn "+turn+" and you, the "+empire+ " empire, have: \n"+cities.length+" city\n"+gold+ " thousand gold\n"+resources+" tons of resources\n";}
-   	if (happiness>0){ currentStatus2 = happiness+" thousand happy citizens\n";}
-   	if (happiness<0){ currentStatus2 = happiness+" thousand angry citizens\n";}
-   	if (happiness==0){ currentStatus2 = "Your people are indifferent to your rule\n";}
-   	if (troops==0){currentStatus3 = "no army\n";}
-   	if (troops>0){currentStatus3 = troops+" thousand soldiers\n";}
-   	if (tech==0){currentStatus4 = "and no technology past the stone age\n";}
-		if (tech>0){currentStatus4 = "and "+tech+" technologies past the stone age\n";}
-    return currentStatus1+currentStatus2+currentStatus3+currentStatus4;
-	}
-  public static void preformAction(){
-    boolean correct = false;
-    if (cities.length==1){ System.out.println("On your turn you can:\nSettle a City [1]\nBuild Your Army [3]\nResearch Technology [4]\nAtttack An Enemy City [5] \nor End Your Turn [6]");}
-    else {System.out.println("On your turn you can:\nSettle a City [1]\nDemolish a City [2]\nBuild Your Army [3]\nResearch Technology [4]\nAtttack An Enemy City [5] \nor End Your Turn [6]");}
-    while (!correct){   
-      System.out.println("Please enter a number 1-6");
-      int choice = scan.nextInt();
-      if (choice == 1){
-        settleCity();
-        correct = true;}
-      if (choice == 2){
-        demolishCity();
-        correct = true;}
-      if (choice ==3){
-        buildArmy();
-        correct = true;}
-      if (choice ==4){
-        researchTech();
-        correct = true;}
-      if (choice ==5){
-        attackCity();
-        correct = true;}
-      if (choice ==6){
-        endTurn();
-        correct = true;}
-    }
-  }
-  public static void settleCity(){
-
-  }
-  public static void demolishCity(){
-
-  }
-  public static void buildArmy(){
-
-  }
-  public static void researchTech(){
-
-  }
-  public static void attackCity(){
-
-  }
-  public static void endTurn(){
-
-  }
 }
->>>>>>> 84177aa3da1ef3655de4b8b060048dc518580df1
