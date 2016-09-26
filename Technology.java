ae7a067d95c3a1a42dc8b3fdc1d4bfa88ce51192
byarmowich <byarm993@gmail.com>
2016-09-26 10:55:54 -0400

    Finished Technology.java

---
+++ Technology.java
@@ -0,0 +1,41 @@
+public class Technology{
+	private int understanding = 0;
+	private boolean foundMeaningOfLife = false;
+	private int experienceLevel = 0;
+	private boolean builtWonderOfTheWorld = false;
+
+	public void philosophize(){
+		understanding = understanding + 25;
+		checkMeaningOfLife();
+	}
+
+	public void improveWriting(){
+		understanding = understanding + 10;
+		checkMeaningOfLife();
+	}
+
+	private void checkMeaningOfLife(){
+		if (understanding > 200){
+			foundMeaningOfLife = true;
+		}
+	}
+
+	public void increaseExperience(int expIncrease){
+		experienceLevel = experienceLevel + expIncrease;
+		checkWorldWonder();
+	}
+
+	private void checkWorldWonder(){
+		if (experienceLevel > 200){
+			builtWonderOfTheWorld = true;
+		}
+	}
+
+	public boolean hasTechnologyWin(){
+		if (foundMeaningOfLife && builtWonderOfTheWorld){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
